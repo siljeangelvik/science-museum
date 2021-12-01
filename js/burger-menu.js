1b6eebc2bfc5de5480d363b78d9e3978811ca36b
siljeangelvik <angelviksilje@gmail.com>
2021-12-01 13:25:01 +0100

    playing around, exhibition layout is ok

---
+++ js/burger-menu.js
@@ -0,0 +1,8 @@
+const toggleableElement = document.getElementById("toggle-element");
+const toggleButton = document.getElementById("toggle-button");
+let toggle = true;
+
+toggleButton.addEventListener("click", () => {
+    toggle = !toggle;
+    toggleableElement.style.display = toggle ? "block" : "none";
+});
