5fcb9b7a05096315f441ada289d941f3e75d2f6c
siljeangelvik <angelviksilje@gmail.com>
2021-12-15 17:50:48 +0100

    next: validator wcag check images alt-text check metas optimalise some more photos
    double check requirements

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -7,3 +7,23 @@ toggleButton.addEventListener("click", () => {
     toggleableElement.style.display = toggle ? "none" : "block";
 
 });
+
+
+let accessibility = document.getElementById("accessibility"),
+    hours = document.getElementById("hours"),
+    kids = document.getElementById("for-kids"),
+    teachers = document.getElementById("for-teachers"),
+    support = document.getElementById("support"),
+    prices = document.getElementById("prices"),
+    location = document.getElementById("museum-location"),
+    events = document.getElementById("events")
+
+function scrollToElement(element, offset = -10) {
+    const y = element.getBoundingClientRect().top + window.pageYOffset + offset;
+
+    window.scrollTo({
+        behavior: 'smooth',
+        top: y,
+    });
+}
+
