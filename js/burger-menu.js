3a61c780882de233da1b4963e86471404f5ad934
siljeangelvik <angelviksilje@gmail.com>
2021-12-15 11:46:32 +0100

    details left

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -7,18 +7,3 @@ toggleButton.addEventListener("click", () => {
     toggleableElement.style.display = toggle ? "none" : "block";
 
 });
-let target = window.location.hash
-
-target = target.replace("#", "");
-
-window.location.hash = "";
-
-$(window).on("load", function () {
-    if (target) {
-        $("html, body").animate({
-            scrollTop: $("#" + target).offset().top
-        }, 700, "swing", function () {
-
-        });
-    }
-});
