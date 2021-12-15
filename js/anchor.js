b4cdbcf1dfee23e426cef67254521731cf323d0b
siljeangelvik <angelviksilje@gmail.com>
2021-12-15 18:06:49 +0100

    next: clean up css validator wcag check images alt-text check metas optimalise some more photos
    double check requirements
    
    try to make js work

---
+++ js/anchor.js
@@ -0,0 +1,17 @@
+let accessibility = document.getElementById("accessibility"),
+    hours = document.getElementById("hours"),
+    kids = document.getElementById("for-kids"),
+    teachers = document.getElementById("for-teachers"),
+    support = document.getElementById("support"),
+    prices = document.getElementById("prices"),
+    location = document.getElementById("museum-location"),
+    events = document.getElementById("events")
+
+function scrollToElement(element, offset = -10) {
+    const y = element.getBoundingClientRect().top + window.pageYOffset + offset;
+
+    window.scrollTo({
+        behavior: 'smooth',
+        top: y,
+    });
+}
